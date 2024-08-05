d10c9e515b577b8d3f669032e3e55d6562370f80
oliveiraordep <p.duarteoliveira@gmail.com>
2024-08-05 13:08:50 -0300

    first commit

---
+++ src/main/java/io/oliveiraordep/agregadordeinvestimentos/entities/User.java
@@ -0,0 +1,33 @@
+package io.oliveiraordep.agregadordeinvestimentos.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = {"username", "email", "password"})
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String username;
+    private String email;
+    private String password;
+
+    @CreationTimestamp
+    private Instant creationTimestamp;
+    @UpdateTimestamp
+    private Instant updateTimestamp;
+
+}
